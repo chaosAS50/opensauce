d8b7b2b1e3d9c242725a7ae8c1a1f077b9ed2b5e
testname <testemail>
2023-12-07 19:57:27 +0900

    add png file

--- Tabletime/Tabletime/Form1.Designer.cs
+++ Tabletime/Tabletime/Form1.Designer.cs
@@ -42,6 +42,7 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Image = global::Tabletime.Properties.Resources._4663997;
             this.pictureBox1.Location = new System.Drawing.Point(140, 65);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(104, 97);
